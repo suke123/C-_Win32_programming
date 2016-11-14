62616508c9d65135f1fb478e72320161959f9711
Takanori Sukekiyo <14ec066@ms.dendai.ac.jp>
2016-11-14 15:16:22 +0900

    編集

--- IphoneImageReflect/Iir01.cpp
+++ IphoneImageReflect/Iir01.cpp
@@ -1,15 +1,14 @@
-/*---------------------------------------------
-Iphone7plus.cpp
------------------------------------------------*/
+/*------------------------------------------------------------------------
+lir01.cpp	(IphoneImageReflection)	 catを動かす
+---------------------------------------------------------------------------*/
 
-#define _USE_MATH_DEFINES
+#include <windows.h>
+#include <tchar.h>
+
 #define	ID_MYTIMER	(32767)
 #define	ID_MYCHILD	(100)
 #define	CHD_WIDTH	(300)
 #define	CHD_HEIGHT	(150)
-#include <windows.h>
-#include <tchar.h>
-#include <math.h>
 
 // プロトタイプ宣言
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
@@ -18,90 +17,23 @@ LRESULT CALLBACK ChdProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 BOOL InitApp(HINSTANCE, WNDPROC, LPCTSTR);
 BOOL InitInstance(HINSTANCE, int, LPCTSTR);
 
-// 大域変数
-static TCHAR szWindowClass[] = _T("iPhone7Plus");
+static TCHAR szClassName[] = _T("Enterprise_Reflect");
 static TCHAR szchClassName[] = _T("child");
-static TCHAR szTitle[] = _T("Reflect cat");
+static TCHAR szTitle[] = _T("SSN Enterprise");
 
-int count = 1;
-HINSTANCE	hInst;
+int count = 1;      //初期動作を指定
 
-int window_pos_x = 100;
-int window_pos_y = 100;
-
-//iPhoneを横向きにする
-int window_width = 690;
-int window_height = 370;
-
-int WINAPI WinMain(HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR pCmdLine,
-	int nCmdShow) {
-
-	WNDCLASSEX wcex;
-
-	// ウィンドウクラスの情報を設定
-	wcex.cbSize = sizeof(WNDCLASSEX);				// 構造体サイズ
-	wcex.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;	// スタイル
-	wcex.lpfnWndProc = WndProc;							// ウィンドウプロシージャ
-	wcex.cbClsExtra = 0;									// 拡張情報１
-	wcex.cbWndExtra = 0;									// 拡張情報２
-	wcex.hInstance = hInstance;							// インスタンスハンドル
-	wcex.hIcon = (HICON)LoadImage(					// アイコン
-		NULL,
-		MAKEINTRESOURCE(IDI_APPLICATION),
-		IMAGE_ICON,
-		0,
-		0,
-		LR_DEFAULTSIZE | LR_SHARED
-		);
-	wcex.hIconSm = wcex.hIcon;							// 子アイコン
-	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	wcex.lpszMenuName = NULL;							// メニュー名
-	wcex.lpszClassName = szWindowClass;
-	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
-
-	// ウィンドウクラスを登録する
-	if (!RegisterClassEx(&wcex)) {
-		MessageBox(NULL,
-			_T("ウィンドウの登録に失敗しました"),
-			_T("ウィンドウの登録エラー"),
-			NULL);
-		return 1;
-	}
-
-	hInst = hInstance; // インスタンス･ハンドルを大域変数に格納
-
-	// ウィンドウを作成する
-	HWND hWnd = CreateWindow(
-		szWindowClass,				// ウィンドウクラス名
-		szTitle,					// タイトルバーに表示する文字列
-		WS_OVERLAPPEDWINDOW,		// ウィンドウの種類
-		window_pos_x,				// ウィンドウを表示する位置(X座標)
-		window_pos_y,				// ウィンドウを表示する位置(Y座標)
-		window_width,				// ウィンドウの幅
-		window_height,				// ウィンドウの高さ
-		NULL,						// 親ウィンドウのウィンドウハンドル
-		NULL,						// メニューハンドル
-		hInst,						// インスタンスハンドル
-		NULL						// その他の作成データ
-		);
-
-	if (!hWnd) {
-		MessageBox(NULL,
-			_T("ウィンドウの作成に失敗しました"),
-			_T("ウィンドウの作成エラー"),
-			NULL);
-		return 1;
-	}
-
-	// ウィンドウを表示する
-	ShowWindow(hWnd, nCmdShow);
-	UpdateWindow(hWnd);
-
-	// メッセージ･ループ
+int WINAPI WinMain(HINSTANCE hCurInst, HINSTANCE hPrevInst, LPSTR lpsCmdLine, int nCmdShow) {
 	MSG msg;
+
+	if (!hPrevInst) {
+		if (!InitApp(hCurInst, WndProc, szClassName)) {
+			return FALSE;
+		}
+	}
+	if (!InitInstance(hCurInst, nCmdShow, szClassName)) {
+		return FALSE;
+	}
 	while (GetMessage(&msg, NULL, 0, 0)) {
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
@@ -109,20 +41,57 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	return (int)msg.wParam;
 }
 
+BOOL InitApp(HINSTANCE hInst, WNDPROC WndProc, LPCTSTR szClassName) {
+	WNDCLASS wc;
+
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInst;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = szClassName;
+
+	return (RegisterClass(&wc));
+}
+
+
+BOOL InitInstance(HINSTANCE hInst, int nCmdShow, LPCTSTR szClassName) {
+	HWND hWnd;
+
+	hWnd = CreateWindow(
+		szClassName,
+		szTitle,						//タイトルバーにこの名前が表示されます
+		WS_OVERLAPPEDWINDOW,	//ウィンドウの種類
+		CW_USEDEFAULT,			//Ｘ座標　適宜指定する
+		CW_USEDEFAULT,			//Ｙ座標　適宜指定する
+		CW_USEDEFAULT,			//幅	　適宜指定する
+		CW_USEDEFAULT,			//高さ	　適宜指定する
+		NULL,						//親ウィンドウのハンドル、親を作るときはNULL
+		NULL,			//メニューハンドル、クラスメニューを使うときはNULL
+		hInst,			//インスタンスハンドル
+		NULL);
+
+	if (!hWnd) {
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return TRUE;
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-
-	HDC hDC;
-	HBRUSH  hBrushBlack;
-	HBRUSH  hBrushYellow;
-	HBRUSH  hBrushWhite;
-	PAINTSTRUCT ps;
-	static LOGFONT logfont;  //フォント情報の構造体
-
 	RECT		rc;
 	static int 	x;
 	static int 	y;
 	static int 	direction;
 	static HWND		hChdWnd;
+	HINSTANCE		hInst;
 	enum { right, left };
 	int height = 450;
 	int width = 900;
@@ -130,50 +99,35 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 	//static int pos_x;   //反射する時にいるx座標
 	//static int pos_y;   //反射する時にいるy座標
 
-	int go_x1 = window_width / 32;
-	int go_y1 = window_height / 64;
-	int go_x2 = window_width / 128 + 1;
-	int go_y2 = window_height / 64;
+	int go_x1 = CHD_WIDTH / 32;
+	int go_y1 = CHD_HEIGHT / 64;
+	int go_x2 = CHD_WIDTH / 128 + 1;
+	int go_y2 = CHD_HEIGHT / 64;
 
 	switch (message) {
 	case WM_CREATE:
+		hInst = ((LPCREATESTRUCT)lParam)->hInstance;
+		InitApp(hInst, ChdProc, szchClassName);
 
-		ZeroMemory(&logfont, sizeof(logfont));	// フォント情報構造体を０で初期化
-		logfont.lfCharSet = DEFAULT_CHARSET;	// システムのデフォルト文字セットを使う
-		wsprintf(logfont.lfFaceName, _T("Times New Roman"));
+		hChdWnd = CreateWindow(
+			szchClassName,			// ウィンドウクラス名
+			NULL,					// タイトルバーに表示する文字列
+			WS_CHILD,				// ウィンドウの種類
+			0,						// ウィンドウを表示する位置(X座標）
+			0,						// ウィンドウを表示する位置(Y座標）
+			CHD_WIDTH,			// ウィンドウの幅
+			CHD_HEIGHT,			// ウィンドウの高さ
+			hWnd,					// 親ウィンドウのウィンドウハンドル
+			(HMENU)ID_MYCHILD,	// メニューハンドル
+			hInst,					// インスタンスハンドル
+			NULL					// その他の作成データ
+			);
+
+		ShowWindow(hChdWnd, SW_SHOW);
+		UpdateWindow(hChdWnd);
+
+		SetTimer(hWnd, ID_MYTIMER, 10, NULL);
 		break;
-
-	case WM_PAINT:
-					  
-		hDC = BeginPaint(hWnd, &ps);		// GDI関数による描画を開始する
-
-		//***********************
-		//**iPhoneを横向きにする
-		//***********************
-		hBrushBlack = CreateSolidBrush(RGB(0, 0, 0));
-		SelectObject(hDC, hBrushBlack);
-		RoundRect(hDC, 10, 10, 643, 322, 70, 80);    //iPhone7の外枠
-		Rectangle(hDC, 85, 321, 110, 325);               //マナーモードON/OFFキー
-		Rectangle(hDC, 135, 321, 185, 325);               //音量＋ボタン
-		Rectangle(hDC, 200, 321, 250, 325);               //音量－ボタン
-		Rectangle(hDC, 120, 7, 170, 20);               //電源ボタン
-
-		hBrushWhite = CreateSolidBrush(RGB(255, 255, 255));
-		SelectObject(hDC, hBrushWhite);
-		Ellipse(hDC, 25, 162, 33, 170);              //中央上部の丸
-		Ellipse(hDC, 35, 221, 50, 236);              //中央上部下の丸
-		RoundRect(hDC, 39, 129, 46, 207, 80, 10);
-		Ellipse(hDC, 579, 139, 633, 193);            //ボタンの外丸
-		
-		hBrushYellow = CreateSolidBrush(RGB(255, 255, 0));
-		SelectObject(hDC, hBrushYellow);
-		Rectangle(hDC, 83, 29, 569, 303);            //iPhone7の画面サイズ
-		
-		SelectObject(hDC, hBrushBlack);
-		Ellipse(hDC, 584, 144, 628, 188);            //ボタンの内丸
-
-		EndPaint(hWnd, &ps);				// GDI関数による描画を終了する
-	break;
 
 	case WM_TIMER:
 		GetClientRect(hWnd, &rc);
@@ -186,7 +140,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 			**/
 		case right:
 			if (count == 1){
-				x += go_x1;
+				x += go_x1;     //
 				y += go_y1;
 			}
 			if (count == 2){
@@ -240,6 +194,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 LRESULT CALLBACK ChdProc(HWND hChdWnd, UINT message, WPARAM wParam, LPARAM lParam) {
 	static HBITMAP	hBitmap;
 	static HBITMAP	hPrevBitmap;
+	HINSTANCE		hInst;
 	PAINTSTRUCT	ps;
 	HDC			hDC;
 	HDC			hCompatDC;
@@ -250,7 +205,7 @@ LRESULT CALLBACK ChdProc(HWND hChdWnd, UINT message, WPARAM wParam, LPARAM lPara
 
 		hBitmap = (HBITMAP)LoadImage(
 			hInst,
-			_T("cat.bmp"),
+			_T("Discovery.bmp"),
 			IMAGE_BITMAP,
 			0,
 			0,
