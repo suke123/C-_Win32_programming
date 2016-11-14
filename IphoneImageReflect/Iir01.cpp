bc54969427601dde3d87ee6866a74ee1a31d2103
Takanori Sukekiyo <14ec066@ms.dendai.ac.jp>
2016-11-14 10:05:05 +0900

    編集

--- IphoneImageReflect/Iir01.cpp
+++ IphoneImageReflect/Iir01.cpp
@@ -3,18 +3,28 @@ Iphone7plus.cpp
 -----------------------------------------------*/
 
 #define _USE_MATH_DEFINES
+#define	ID_MYTIMER	(32767)
+#define	ID_MYCHILD	(100)
+#define	CHD_WIDTH	(300)
+#define	CHD_HEIGHT	(150)
 #include <windows.h>
 #include <tchar.h>
 #include <math.h>
 
 // プロトタイプ宣言
+LRESULT CALLBACK WindProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK ChdProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
+BOOL InitApp(HINSTANCE, WNDPROC, LPCTSTR);
+BOOL InitInstance(HINSTANCE, int, LPCTSTR);
+
 // 大域変数
 static TCHAR szWindowClass[] = _T("iPhone7Plus");
-static TCHAR szTitle[] = _T("iPhone7Plus");
+static TCHAR szchClassName[] = _T("child");
+static TCHAR szTitle[] = _T("Reflect cat");
 
+int count = 1;
 HINSTANCE	hInst;
 
 int window_pos_x = 100;
@@ -101,7 +111,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
 }
 
 // ウィンドウプロシージャ
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK WindProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
 	HDC hDC;
 	HBRUSH  hBrushBlack;
 	HBRUSH  hBrushYellow;
@@ -155,6 +165,158 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 	default:
 		return DefWindowProc(hWnd, message, wParam, lParam);
 		break;
+	}
+
+	return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	RECT		rc;
+	static int 	x;
+	static int 	y;
+	static int 	direction;
+	static HWND		hChdWnd;
+	enum { right, left };
+	int height = 450;
+	int width = 900;
+	bool window_end = false;
+	//static int pos_x;   //反射する時にいるx座標
+	//static int pos_y;   //反射する時にいるy座標
+
+	int go_x1 = window_width / 32;
+	int go_y1 = window_height / 64;
+	int go_x2 = window_width / 128 + 1;
+	int go_y2 = window_height / 64;
+
+	switch (message) {
+	case WM_CREATE:
+		/*hInst = ((LPCREATESTRUCT)lParam)->hInstance;
+		InitApp(hInst, ChdProc, szchClassName);
+
+		/*hChdWnd = CreateWindow(
+			szchClassName,			// ウィンドウクラス名
+			NULL,					// タイトルバーに表示する文字列
+			WS_CHILD,				// ウィンドウの種類
+			0,						// ウィンドウを表示する位置(X座標）
+			0,						// ウィンドウを表示する位置(Y座標）
+			CHD_WIDTH,			// ウィンドウの幅
+			CHD_HEIGHT,			// ウィンドウの高さ
+			hWnd,					// 親ウィンドウのウィンドウハンドル
+			(HMENU)ID_MYCHILD,	// メニューハンドル
+			hInst,					// インスタンスハンドル
+			NULL					// その他の作成データ
+			);*/
+
+		ShowWindow(hChdWnd, SW_SHOW);
+		UpdateWindow(hChdWnd);
+
+		SetTimer(hWnd, ID_MYTIMER, 10, NULL);
+		break;
+
+	case WM_TIMER:
+		GetClientRect(hWnd, &rc);
+		MoveWindow(hChdWnd, x, y, CHD_WIDTH, CHD_HEIGHT, TRUE);
+
+		switch (direction) {
+
+			/**
+			**右向きに移動する時の処理
+			**/
+		case right:
+			if (count == 1){
+				x += go_x1;     //
+				y += go_y1;
+			}
+			if (count == 2){
+				x += go_x2;
+				y -= go_y2;
+			}
+			if (y <= 0){
+				x += go_x2;
+				y += go_y2;
+			}
+			//if (x >= rc.right - CHD_WIDTH || y >= height) {
+			if (x >= rc.right - CHD_WIDTH){
+				count = 2;
+				direction = left;
+			}
+
+			break;
+
+			/**
+			**左向きに移動する時の処理
+			**/
+		case left:
+			if (x > CHD_WIDTH / 2){
+				x -= go_x2;
+				y += go_y2;
+			}
+			if (y >= rc.bottom - CHD_HEIGHT){
+				x += go_x2;
+				y -= go_y2;
+			}
+			if (x < 0 || y < 0){
+				direction = right;
+			}
+			break;
+		}
+		break;
+
+	case WM_DESTROY:
+		KillTimer(hWnd, ID_MYTIMER);
+		PostQuitMessage(0);
+		break;
+
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+		break;
+	}
+
+	return 0;
+}
+
+LRESULT CALLBACK ChdProc(HWND hChdWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	static HBITMAP	hBitmap;
+	static HBITMAP	hPrevBitmap;
+	PAINTSTRUCT	ps;
+	HDC			hDC;
+	HDC			hCompatDC;
+
+	switch (message) {
+	case WM_PAINT:
+		hInst = (HINSTANCE)GetWindowLong(hChdWnd, GWL_HINSTANCE);
+
+		hBitmap = (HBITMAP)LoadImage(
+			hInst,
+			_T("cat.bmp"),
+			IMAGE_BITMAP,
+			0,
+			0,
+			LR_LOADFROMFILE);
+
+		if (hBitmap == NULL) {
+			MessageBox(
+				hChdWnd,
+				_T("ビットマップのロードに失敗しました"),
+				_T("エラー"),
+				MB_OK | MB_ICONWARNING
+				);
+			return 0;
+		}
+
+		hDC = BeginPaint(hChdWnd, &ps);
+		hCompatDC = CreateCompatibleDC(hDC);
+		SelectObject(hCompatDC, hBitmap);
+
+		BitBlt(hDC, 0, 0, CHD_WIDTH, CHD_HEIGHT, hCompatDC, 0, 0, SRCCOPY);
+
+		DeleteDC(hCompatDC);
+		DeleteObject(hBitmap);
+		EndPaint(hChdWnd, &ps);
+		break;
+
+	default:
+		return(DefWindowProc(hChdWnd, message, wParam, lParam));
 	}
 	return 0;
 }
