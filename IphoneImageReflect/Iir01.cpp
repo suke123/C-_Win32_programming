8242a5e67da471f1c42507c85d12c6d74377349d
Takanori Sukekiyo <14ec066@ms.dendai.ac.jp>
2016-11-14 10:33:45 +0900

    編集

--- IphoneImageReflect/Iir01.cpp
+++ IphoneImageReflect/Iir01.cpp
@@ -12,7 +12,6 @@ Iphone7plus.cpp
 #include <math.h>
 
 // プロトタイプ宣言
-LRESULT CALLBACK WindProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK ChdProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
@@ -76,17 +75,17 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
 	// ウィンドウを作成する
 	HWND hWnd = CreateWindow(
-		szWindowClass,					// ウィンドウクラス名
-		szTitle,							// タイトルバーに表示する文字列
+		szWindowClass,				// ウィンドウクラス名
+		szTitle,					// タイトルバーに表示する文字列
 		WS_OVERLAPPEDWINDOW,		// ウィンドウの種類
-		window_pos_x,								// ウィンドウを表示する位置(X座標)
-		window_pos_y,								// ウィンドウを表示する位置(Y座標)
-		window_width,								// ウィンドウの幅
-		window_height,								// ウィンドウの高さ
-		NULL,							// 親ウィンドウのウィンドウハンドル
-		NULL,							// メニューハンドル
-		hInst,							// インスタンスハンドル
-		NULL							// その他の作成データ
+		window_pos_x,				// ウィンドウを表示する位置(X座標)
+		window_pos_y,				// ウィンドウを表示する位置(Y座標)
+		window_width,				// ウィンドウの幅
+		window_height,				// ウィンドウの高さ
+		NULL,						// 親ウィンドウのウィンドウハンドル
+		NULL,						// メニューハンドル
+		hInst,						// インスタンスハンドル
+		NULL						// その他の作成データ
 		);
 
 	if (!hWnd) {
@@ -110,8 +109,8 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	return (int)msg.wParam;
 }
 
-// ウィンドウプロシージャ
-LRESULT CALLBACK WindProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+
 	HDC hDC;
 	HBRUSH  hBrushBlack;
 	HBRUSH  hBrushYellow;
@@ -119,58 +118,6 @@ LRESULT CALLBACK WindProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	PAINTSTRUCT ps;
 	static LOGFONT logfont;  //フォント情報の構造体
 
-	switch (message) {
-	case WM_CREATE:
-		ZeroMemory(&logfont, sizeof(logfont));	// フォント情報構造体を０で初期化
-		logfont.lfCharSet = DEFAULT_CHARSET;	// システムのデフォルト文字セットを使う
-		wsprintf(logfont.lfFaceName, _T("Times New Roman"));
-		break;
-
-	case WM_PAINT:{
-
-					  hDC = BeginPaint(hWnd, &ps);		// GDI関数による描画を開始する
-
-					  //***********************
-					  //**iPhoneを横向きにする
-					  //***********************
-					  hBrushBlack = CreateSolidBrush(RGB(0, 0, 0));
-					  SelectObject(hDC, hBrushBlack);
-					  RoundRect(hDC, 10, 10, 643, 322, 70, 80);    //iPhone7の外枠
-					  Rectangle(hDC, 85, 321, 110, 325);               //マナーモードON/OFFキー
-					  Rectangle(hDC, 135, 321, 185, 325);               //音量＋ボタン
-					  Rectangle(hDC, 200, 321, 250, 325);               //音量－ボタン
-					  Rectangle(hDC, 120, 7, 170, 20);               //電源ボタン
-
-					  hBrushWhite = CreateSolidBrush(RGB(255, 255, 255));
-					  SelectObject(hDC, hBrushWhite);
-					  Ellipse(hDC, 25, 162, 33, 170);              //中央上部の丸
-					  Ellipse(hDC, 35, 221, 50, 236);              //中央上部下の丸
-					  RoundRect(hDC, 39, 129, 46, 207, 80, 10);
-					  Ellipse(hDC, 579, 139, 633, 193);            //ボタンの外丸
-
-					  hBrushYellow = CreateSolidBrush(RGB(255, 255, 0));
-					  SelectObject(hDC, hBrushYellow);
-					  Rectangle(hDC, 83, 29, 569, 303);            //iPhone7の画面サイズ
-
-					  SelectObject(hDC, hBrushBlack);
-					  Ellipse(hDC, 584, 144, 628, 188);            //ボタンの内丸
-
-					  EndPaint(hWnd, &ps);				// GDI関数による描画を終了する
-	}break;
-
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
-		break;
-	}
-
-	return 0;
-}
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
 	RECT		rc;
 	static int 	x;
 	static int 	y;
@@ -190,28 +137,43 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 
 	switch (message) {
 	case WM_CREATE:
-		/*hInst = ((LPCREATESTRUCT)lParam)->hInstance;
-		InitApp(hInst, ChdProc, szchClassName);
 
-		/*hChdWnd = CreateWindow(
-			szchClassName,			// ウィンドウクラス名
-			NULL,					// タイトルバーに表示する文字列
-			WS_CHILD,				// ウィンドウの種類
-			0,						// ウィンドウを表示する位置(X座標）
-			0,						// ウィンドウを表示する位置(Y座標）
-			CHD_WIDTH,			// ウィンドウの幅
-			CHD_HEIGHT,			// ウィンドウの高さ
-			hWnd,					// 親ウィンドウのウィンドウハンドル
-			(HMENU)ID_MYCHILD,	// メニューハンドル
-			hInst,					// インスタンスハンドル
-			NULL					// その他の作成データ
-			);*/
-
-		ShowWindow(hChdWnd, SW_SHOW);
-		UpdateWindow(hChdWnd);
-
-		SetTimer(hWnd, ID_MYTIMER, 10, NULL);
+		ZeroMemory(&logfont, sizeof(logfont));	// フォント情報構造体を０で初期化
+		logfont.lfCharSet = DEFAULT_CHARSET;	// システムのデフォルト文字セットを使う
+		wsprintf(logfont.lfFaceName, _T("Times New Roman"));
 		break;
+
+	case WM_PAINT:
+					  
+		hDC = BeginPaint(hWnd, &ps);		// GDI関数による描画を開始する
+
+		//***********************
+		//**iPhoneを横向きにする
+		//***********************
+		hBrushBlack = CreateSolidBrush(RGB(0, 0, 0));
+		SelectObject(hDC, hBrushBlack);
+		RoundRect(hDC, 10, 10, 643, 322, 70, 80);    //iPhone7の外枠
+		Rectangle(hDC, 85, 321, 110, 325);               //マナーモードON/OFFキー
+		Rectangle(hDC, 135, 321, 185, 325);               //音量＋ボタン
+		Rectangle(hDC, 200, 321, 250, 325);               //音量－ボタン
+		Rectangle(hDC, 120, 7, 170, 20);               //電源ボタン
+
+		hBrushWhite = CreateSolidBrush(RGB(255, 255, 255));
+		SelectObject(hDC, hBrushWhite);
+		Ellipse(hDC, 25, 162, 33, 170);              //中央上部の丸
+		Ellipse(hDC, 35, 221, 50, 236);              //中央上部下の丸
+		RoundRect(hDC, 39, 129, 46, 207, 80, 10);
+		Ellipse(hDC, 579, 139, 633, 193);            //ボタンの外丸
+		
+		hBrushYellow = CreateSolidBrush(RGB(255, 255, 0));
+		SelectObject(hDC, hBrushYellow);
+		Rectangle(hDC, 83, 29, 569, 303);            //iPhone7の画面サイズ
+		
+		SelectObject(hDC, hBrushBlack);
+		Ellipse(hDC, 584, 144, 628, 188);            //ボタンの内丸
+
+		EndPaint(hWnd, &ps);				// GDI関数による描画を終了する
+	break;
 
 	case WM_TIMER:
 		GetClientRect(hWnd, &rc);
@@ -224,7 +186,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 			**/
 		case right:
 			if (count == 1){
-				x += go_x1;     //
+				x += go_x1;
 				y += go_y1;
 			}
 			if (count == 2){
