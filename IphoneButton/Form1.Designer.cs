8b1610385203b41d09254e267772e3af62d01b31
Takanori Sukekiyo <14ec066@ms.dendai.ac.jp>
2016-12-24 20:58:00 +0900

    編集

--- IphoneButton/Form1.Designer.cs
+++ IphoneButton/Form1.Designer.cs
@@ -28,20 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(104, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 58);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(170, 98);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(60, 58);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(236, 98);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(60, 58);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(236, 480);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(60, 58);
+            this.button17.TabIndex = 19;
+            this.button17.Text = "button17";
+            this.button17.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(170, 480);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(60, 58);
+            this.button18.TabIndex = 18;
+            this.button18.Text = "button18";
+            this.button18.UseVisualStyleBackColor = true;
+            // 
+            // button19
+            // 
+            this.button19.Location = new System.Drawing.Point(104, 480);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(60, 58);
+            this.button19.TabIndex = 17;
+            this.button19.Text = "button19";
+            this.button19.UseVisualStyleBackColor = true;
+            // 
+            // button20
+            // 
+            this.button20.Location = new System.Drawing.Point(38, 480);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(61, 58);
+            this.button20.TabIndex = 16;
+            this.button20.Text = "button20";
+            this.button20.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.BackgroundImage = global::IphoneButton.Properties.Resources.iphone画面;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(336, 635);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button20);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "IphoneButton";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button20;
     }
 }
 
--- IphoneButton/Form1.cs
+++ IphoneButton/Form1.cs
@@ -16,5 +16,14 @@ namespace IphoneButton
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                    }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
