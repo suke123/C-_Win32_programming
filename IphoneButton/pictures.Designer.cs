23837eb2ed93994886f000fa292c621d0a67fecd
Takanori Sukekiyo <14ec066@ms.dendai.ac.jp>
2016-12-26 19:48:04 +0900

    編集

--- IphoneButton/pictures.Designer.cs
+++ IphoneButton/pictures.Designer.cs
@@ -33,20 +33,23 @@
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.comboBox = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // imageList1
             // 
             this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
             this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
-            this.imageList1.Images.SetKeyName(0, "cat.bmp");
-            this.imageList1.Images.SetKeyName(1, "dog.bmp");
-            this.imageList1.Images.SetKeyName(2, "panda.bmp");
+            this.imageList1.Images.SetKeyName(0, "molmot.jpg");
+            this.imageList1.Images.SetKeyName(1, "elephant.jpg");
+            this.imageList1.Images.SetKeyName(2, "kame.png");
             // 
             // comboBox
             // 
             this.comboBox.FormattingEnabled = true;
-            this.comboBox.Location = new System.Drawing.Point(98, 6);
+            this.comboBox.Location = new System.Drawing.Point(14, 24);
             this.comboBox.Name = "comboBox";
             this.comboBox.Size = new System.Drawing.Size(132, 20);
             this.comboBox.TabIndex = 0;
@@ -61,15 +64,36 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "表示させる画像";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "表示";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(284, 210);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
             // pictures
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox);
             this.Name = "pictures";
             this.Text = "pictures";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,5 +104,7 @@
         private System.Windows.Forms.ImageList imageList1;
         private System.Windows.Forms.ComboBox comboBox;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- IphoneButton/pictures.cs
+++ IphoneButton/pictures.cs
@@ -17,9 +17,32 @@ namespace IphoneButton
             InitializeComponent();
 
             comboBox.Items.Add("選択してください");
-            comboBox.Items.Add("イヌ");
-            comboBox.Items.Add("ネコ");
-            comboBox.Items.Add("パンダ");
+            comboBox.Items.Add("モルモット");
+            comboBox.Items.Add("ゾウ");
+            comboBox.Items.Add("カメ");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String ani = comboBox.SelectedItem.ToString();
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            if (ani == "モルモット")
+            {
+                pictureBox1.Image = imageList1.Images[0];
+            }
+            else if (ani == "ゾウ")
+            {
+                pictureBox1.Image = imageList1.Images[1];
+            }
+            else if (ani == "カメ")
+            {
+                pictureBox1.Image = imageList1.Images[2];
+            }
+            else
+            {
+                pictureBox1.Image = null;
+            }
         }
     }
 }
