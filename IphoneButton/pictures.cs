85c2a98e7dea6e7425f414f1cac57727fe36486d
Takanori Sukekiyo <14ec066@ms.dendai.ac.jp>
2016-12-26 19:59:51 +0900

    編集
    コメントを追加

--- IphoneButton/pictures.cs
+++ IphoneButton/pictures.cs
@@ -16,6 +16,7 @@ namespace IphoneButton
         {
             InitializeComponent();
 
+            //選択欄の項目を追加
             comboBox.Items.Add("選択してください");
             comboBox.Items.Add("モルモット");
             comboBox.Items.Add("ゾウ");
@@ -26,22 +27,27 @@ namespace IphoneButton
         private void button1_Click(object sender, EventArgs e)
         {
             String ani = comboBox.SelectedItem.ToString();
-            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; //画像をピクチャーボックスいっぱいに描画する
+
+            //「モルモット」が選択された時の処理
             if (ani == "モルモット")
             {
                 pictureBox1.Image = imageList1.Images[0];
             }
+            //「ゾウ」が選択された時の処理
             else if (ani == "ゾウ")
             {
                 pictureBox1.Image = imageList1.Images[1];
             }
+            //「カメ」が選択された時の処理
             else if (ani == "カメ")
             {
                 pictureBox1.Image = imageList1.Images[2];
             }
+            //「選択してください」が選択された時の処理
             else
             {
-                pictureBox1.Image = null;
+                pictureBox1.Image = null;   //何も描画しない
             }
         }
     }
