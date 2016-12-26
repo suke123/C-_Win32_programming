861bda5736d5fca3a221ab0aa00bacfe0336dc85
Takanori Sukekiyo <14ec066@ms.dendai.ac.jp>
2016-12-26 16:02:29 +0900

    編集
    世界時間完成
    自動で時計が作動するようになった
    ラジオボタンにより、6か国の時間を表示可能になった。

--- IphoneButton/watch.Designer.cs
+++ IphoneButton/watch.Designer.cs
@@ -28,11 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.quitButton = new System.Windows.Forms.Button();
-            this.refreshButton = new System.Windows.Forms.Button();
             this.dateText = new System.Windows.Forms.Label();
             this.timeText = new System.Windows.Forms.Label();
+            this.japan = new System.Windows.Forms.RadioButton();
+            this.america = new System.Windows.Forms.RadioButton();
+            this.london = new System.Windows.Forms.RadioButton();
+            this.china = new System.Windows.Forms.RadioButton();
+            this.france = new System.Windows.Forms.RadioButton();
+            this.australia = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -42,14 +51,14 @@
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
             this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.label1.Size = new System.Drawing.Size(222, 40);
+            this.label1.Size = new System.Drawing.Size(181, 40);
             this.label1.TabIndex = 2;
-            this.label1.Text = "世界の時間";
+            this.label1.Text = "世界時計";
             // 
             // quitButton
             // 
             this.quitButton.Font = new System.Drawing.Font("MS UI Gothic", 12F);
-            this.quitButton.Location = new System.Drawing.Point(157, 217);
+            this.quitButton.Location = new System.Drawing.Point(92, 217);
             this.quitButton.Name = "quitButton";
             this.quitButton.Size = new System.Drawing.Size(94, 32);
             this.quitButton.TabIndex = 3;
@@ -57,48 +66,121 @@
             this.quitButton.UseVisualStyleBackColor = true;
             this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
             // 
-            // refreshButton
-            // 
-            this.refreshButton.BackColor = System.Drawing.SystemColors.Control;
-            this.refreshButton.Font = new System.Drawing.Font("MS UI Gothic", 12F);
-            this.refreshButton.Location = new System.Drawing.Point(30, 217);
-            this.refreshButton.Name = "refreshButton";
-            this.refreshButton.Size = new System.Drawing.Size(94, 31);
-            this.refreshButton.TabIndex = 4;
-            this.refreshButton.Text = "更新";
-            this.refreshButton.UseVisualStyleBackColor = false;
-            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
-            // 
             // dateText
             // 
             this.dateText.AutoSize = true;
-            this.dateText.Font = new System.Drawing.Font("MS UI Gothic", 15F);
-            this.dateText.Location = new System.Drawing.Point(50, 149);
+            this.dateText.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold);
+            this.dateText.Location = new System.Drawing.Point(41, 151);
             this.dateText.Name = "dateText";
-            this.dateText.Size = new System.Drawing.Size(0, 20);
+            this.dateText.Size = new System.Drawing.Size(195, 20);
             this.dateText.TabIndex = 5;
+            this.dateText.Text = "0000年00月00日(〇)";
             // 
             // timeText
             // 
             this.timeText.AutoSize = true;
-            this.timeText.Font = new System.Drawing.Font("MS UI Gothic", 15F);
-            this.timeText.Location = new System.Drawing.Point(50, 181);
+            this.timeText.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold);
+            this.timeText.Location = new System.Drawing.Point(91, 184);
             this.timeText.Name = "timeText";
-            this.timeText.Size = new System.Drawing.Size(0, 20);
+            this.timeText.Size = new System.Drawing.Size(97, 20);
             this.timeText.TabIndex = 6;
+            this.timeText.Text = "00：00：00";
+            // 
+            // japan
+            // 
+            this.japan.AutoSize = true;
+            this.japan.Checked = true;
+            this.japan.Location = new System.Drawing.Point(18, 18);
+            this.japan.Name = "japan";
+            this.japan.Size = new System.Drawing.Size(77, 16);
+            this.japan.TabIndex = 8;
+            this.japan.TabStop = true;
+            this.japan.Text = "日本・東京";
+            this.japan.UseVisualStyleBackColor = true;
+            // 
+            // america
+            // 
+            this.america.AutoSize = true;
+            this.america.Location = new System.Drawing.Point(18, 40);
+            this.america.Name = "america";
+            this.america.Size = new System.Drawing.Size(115, 16);
+            this.america.TabIndex = 9;
+            this.america.Text = "アメリカ・ニューヨーク";
+            this.america.UseVisualStyleBackColor = true;
+            // 
+            // london
+            // 
+            this.london.AutoSize = true;
+            this.london.Location = new System.Drawing.Point(151, 61);
+            this.london.Name = "london";
+            this.london.Size = new System.Drawing.Size(100, 16);
+            this.london.TabIndex = 10;
+            this.london.Text = "イギリス・ロンドン";
+            this.london.UseVisualStyleBackColor = true;
+            // 
+            // china
+            // 
+            this.china.AutoSize = true;
+            this.china.Location = new System.Drawing.Point(151, 18);
+            this.china.Name = "china";
+            this.china.Size = new System.Drawing.Size(77, 16);
+            this.china.TabIndex = 11;
+            this.china.Text = "中国・北京";
+            this.china.UseVisualStyleBackColor = true;
+            // 
+            // france
+            // 
+            this.france.AutoSize = true;
+            this.france.Location = new System.Drawing.Point(151, 40);
+            this.france.Name = "france";
+            this.france.Size = new System.Drawing.Size(80, 16);
+            this.france.TabIndex = 12;
+            this.france.Text = "フランス・パリ";
+            this.france.UseVisualStyleBackColor = true;
+            // 
+            // australia
+            // 
+            this.australia.AutoSize = true;
+            this.australia.Location = new System.Drawing.Point(18, 61);
+            this.australia.Name = "australia";
+            this.australia.Size = new System.Drawing.Size(127, 16);
+            this.australia.TabIndex = 13;
+            this.australia.Text = "オーストラリア・シドニー";
+            this.australia.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.france);
+            this.groupBox1.Controls.Add(this.australia);
+            this.groupBox1.Controls.Add(this.japan);
+            this.groupBox1.Controls.Add(this.america);
+            this.groupBox1.Controls.Add(this.china);
+            this.groupBox1.Controls.Add(this.london);
+            this.groupBox1.Location = new System.Drawing.Point(12, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 83);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "国・都市";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // watch
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.timeText);
             this.Controls.Add(this.dateText);
-            this.Controls.Add(this.refreshButton);
             this.Controls.Add(this.quitButton);
             this.Controls.Add(this.label1);
             this.Name = "watch";
             this.Text = "watch";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,9 +190,16 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button quitButton;
-        private System.Windows.Forms.Button refreshButton;
         private System.Windows.Forms.Label dateText;
         private System.Windows.Forms.Label timeText;
+        private System.Windows.Forms.RadioButton japan;
+        private System.Windows.Forms.RadioButton america;
+        private System.Windows.Forms.RadioButton london;
+        private System.Windows.Forms.RadioButton china;
+        private System.Windows.Forms.RadioButton france;
+        private System.Windows.Forms.RadioButton australia;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer timer1;
 
 
     }
--- IphoneButton/watch.cs
+++ IphoneButton/watch.cs
@@ -15,20 +15,63 @@ namespace IphoneButton
         public watch()
         {
             InitializeComponent();
-
-            dateText.Text = DateTime.Now.ToString("yyyy年MM月dd日(ddd)");
-            timeText.Text = DateTime.Now.ToString("hh：mm：ss");
+            timer1.Start();          //時計を進める
         }
 
+        //「閉じる」ボタンを押すと、フォームを閉じる
         private void quitButton_Click(object sender, EventArgs e)
         {
-            this.Close();
+            this.Close();     //フォームを閉じる
         }
 
-        private void refreshButton_Click(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            dateText.Text = DateTime.Now.ToString("yyyy年MM月dd日(ddd)");
-            timeText.Text = DateTime.Now.ToString("hh：mm：ss");
+            //日本時間
+            if (japan.Checked)
+            {
+                dateText.Text = DateTime.Now.ToString("yyyy年MM月dd日(ddd)");
+                timeText.Text = DateTime.Now.ToString("HH：mm：ss");
+            }
+            //ニューヨーク時間
+            else if (america.Checked)
+            {
+                DateTime dt = DateTime.Now;
+                DateTime us_dt = dt.AddHours(-14);  //時差14時間を日本時間から引く
+                dateText.Text = us_dt.ToString("yyyy年MM月dd日(ddd)");
+                timeText.Text = us_dt.ToString("HH：mm：ss");
+            }
+            //シドニー時間
+            else if (australia.Checked)
+            {
+                DateTime dt = DateTime.Now;
+                DateTime us_dt = dt.AddHours(2);    //時差2時間を日本時間に足す
+                dateText.Text = us_dt.ToString("yyyy年MM月dd日(ddd)");
+                timeText.Text = us_dt.ToString("HH：mm：ss");
+            }
+            //北京時間
+            else if (china.Checked)
+            {
+                DateTime dt = DateTime.Now;
+                DateTime us_dt = dt.AddHours(-1);   //時差1時間を日本時間から引く
+                dateText.Text = us_dt.ToString("yyyy年MM月dd日(ddd)");
+                timeText.Text = us_dt.ToString("HH：mm：ss");
+            }
+            //パリ時間
+            else if (france.Checked)
+            {
+                DateTime dt = DateTime.Now;
+                DateTime us_dt = dt.AddHours(-8);   //時差8時間を日本時間から引く
+                dateText.Text = us_dt.ToString("yyyy年MM月dd日(ddd)");
+                timeText.Text = us_dt.ToString("HH：mm：ss");
+            }
+            //ロンドン時間
+            else if (london.Checked)
+            {
+                DateTime dt = DateTime.Now;
+                DateTime us_dt = dt.AddHours(-9);   //時差9時間を日本時間から引く
+                dateText.Text = us_dt.ToString("yyyy年MM月dd日(ddd)");
+                timeText.Text = us_dt.ToString("HH：mm：ss");
+            }
         }
 
     }
